ec1aa0303f9fb52652ae597b94fad8526360ea6d
Agrima Agrawal <84567933+Agrimaagrawal@users.noreply.github.com>
2023-01-29 19:33:21 +0530

    an

--- pointers.cpp
+++ pointers.cpp
@@ -1,10 +1,37 @@
 #include<iostream>
 using namespace std;
+int print1(int *p){
+    cout<<p+1;
+}
+int getSum(int *arr,int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    return sum;
+}
 int  main(){
+    /*
     int a=0;
 
     int *s=&a;
-    cout<<s;
-    bhgu
+    int arr[10]={1,2,3,4,5,6,7,8,9,0};
+    
+    cout<<*(arr+1);
+    cout<<*arr;
+    //in case of int array arr will print address of it
+     char ch[3]="ac";
+     cout<<ch;
+     //in case of char array name of that array will print string written in it
+    cout<<ch[1];
+    cout<<ch[3];
+    */
+    int x=8;
+    int *p=&x;
+    print1(p);
+     int arr[10]={1,2,3,4,5,6,7,8,9,0};
+     cout<<getSum(arr,10);
+
+
     return 0;
 }
